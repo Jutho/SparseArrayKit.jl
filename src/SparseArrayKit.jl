39490aa9989b99db042b3193d6cd492f32030c75
Jutho <jutho.haegeman@ugent.be>
2023-07-24 11:02:38 +0200

    fix path in old style Requires

--- src/SparseArrayKit.jl
+++ src/SparseArrayKit.jl
@@ -21,7 +21,7 @@ include("tensoroperations.jl")
 function __init__()
     @static if !isdefined(Base, :get_extension)
         @require TensorOperations="6aa20fa7-93e2-5fca-9bc0-fbd0db3c71a2" begin
-            include("../ext/SparseArrayTensorOperations.jl")
+            include("../ext/SparseArrayKitTensorOperations.jl")
         end
         
         @require SparseArrays="2f01184e-e22b-5df5-ae63-d93ebab69eaf" begin
