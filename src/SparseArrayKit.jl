b4ac84762832239f0985d4ba7c1a1f95283dc8b1
lkdvos <lukas.devos@ugent.be>
2023-09-13 10:48:14 +0200

    Use PackageExtensionCompat

--- src/SparseArrayKit.jl
+++ src/SparseArrayKit.jl
@@ -21,16 +21,9 @@ include("linearalgebra.jl")
 
 # Initialization
 #-----------------
+using PackageExtensionCompat
 function __init__()
-    @static if !isdefined(Base, :get_extension)
-        @require TensorOperations = "6aa20fa7-93e2-5fca-9bc0-fbd0db3c71a2" begin
-            include("../ext/SparseArrayKitTensorOperations.jl")
-        end
-
-        @require SparseArrays = "2f01184e-e22b-5df5-ae63-d93ebab69eaf" begin
-            include("../ext/SparseArrayKitSparseArrays.jl")
-        end
-    end
+    @require_extensions
 end
 
 end
